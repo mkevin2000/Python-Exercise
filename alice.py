ce37bac5909aae4e3af8b02f7e1bed63c80a3190
mkevin2000 <muhirekevin2000@gmail.com>
2026-04-11 16:10:13 +0200

    Add grade() method to student class

--- alice.py
+++ alice.py
@@ -7,10 +7,20 @@ class Student:
         return self.marks < other.marks
     def __str__(self):
         return f"{self.name} has {self.marks} marks."
+    def grade(self):
+        if self.marks >= 90:
+            return "A"
+        elif self.marks >= 80:
+            return "B"
+        elif self.marks >= 70:
+            return "C"
+        else:
+            return "F"
+
 s1 = Student("Alice", 85)
 s2 = Student("Bob", 90)
 s3 = Student("Charlie", 80)
 s4 = Student("David", 95)
 students = [s1, s2, s3, s4]
 for student in sorted(students):
-    print(student)
+    print (f"{student} Grade: {student.grade()}")
